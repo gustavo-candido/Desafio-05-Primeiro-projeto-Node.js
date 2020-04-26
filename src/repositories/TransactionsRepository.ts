197eb9a5adb5abecdc6b72501f17d388b5b46406
gustavo-candido <gustavocandidofreitas@gmail.com>
2020-04-26 20:24:38 -0300

    Using factory pattern to get balance

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,12 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +20,64 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    // eslint-disable-next-line prettier/prettier
+    const reducerFactory = (type: 'income' | 'outcome'): ((acc: Transaction, curr: Transaction) => Transaction) => {
+      const reducer = (
+        accTransaction: Transaction,
+        transaction: Transaction,
+      ): Transaction => {
+        return new Transaction({
+          title: '',
+          value:
+            accTransaction.value +
+            (transaction.type === type ? transaction.value : 0),
+          type,
+        });
+      };
+
+      return reducer;
+    };
+
+    const reducerIncome = reducerFactory('income');
+    const reducerOutcome = reducerFactory('outcome');
+
+    const { value: income } = this.all().reduce(
+      reducerIncome,
+
+      new Transaction({
+        title: '',
+        value: 0,
+        type: 'income',
+      }),
+    );
+
+    const { value: outcome } = this.all().reduce(
+      reducerOutcome,
+      new Transaction({
+        title: '',
+        value: 0,
+        type: 'outcome',
+      }),
+    );
+
+    return { income, outcome, total: income - outcome };
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const { total } = this.getBalance();
+
+    if (type === 'outcome' && total < value) {
+      throw Error("Can't create a outcome transaction without enought income");
+    }
+    const newTransaction = new Transaction({ title, value, type });
+
+    this.transactions.push(newTransaction);
+
+    return newTransaction;
   }
 }
 
--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,12 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface CreateTransaction {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +14,14 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({ title, value, type }: CreateTransaction): Transaction {
+    const newTransation = this.transactionsRepository.create({
+      title,
+      value,
+      type,
+    });
+
+    return newTransation;
   }
 }
 
